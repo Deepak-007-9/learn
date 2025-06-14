6019118fc6bb5e2e40861ffece2f0236290fabbc
Venkat, M <mutnurivenkatesh@hotmail.com>
2025-06-14 19:05:50 +0530

    added scanf and display

--- prog.c
+++ prog.c
@@ -1,23 +1,31 @@
-// You are using GCC
 #include<stdio.h>
+
 #define MAXSIZE 5
+
 int queue[MAXSIZE],front=-1,rear=-1;
 
 int isEmpty(){
-    front = rear = -1
+    front = rear = -1;
 }
 
 int dequeue(){
     if(isEmpty)
     printf("underflow");
-    else 
-    
+
 }
 
 
 int main() {
     int choice,value;
-    
-   printf("welcome\n");
+
+    for (int i = 0; i < MAXSIZE; i++) {
+        scanf("%d", &value);
+        queue[i] = value;
+    }
+
+    for (int i = 0; i < MAXSIZE; i++) {
+        printf("%d\n", queue[i]);
+    }
+
     return 0;
 }
